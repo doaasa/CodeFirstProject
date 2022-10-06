49e82d37d7f038cbfdeaf117d448634d641e27da
Doaa Sayed Ibrahim Morsy <76880107+doaasa@users.noreply.github.com>
2022-10-06 12:12:14 +0200

    Add files via upload

---
+++ codefirstproject/Migrations/20220827144322_intial.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace codefirstproject.Migrations
+{
+    public partial class intial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "students",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GPA = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    age = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_students", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "teachers",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Salary = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    age = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_teachers", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "students");
+
+            migrationBuilder.DropTable(
+                name: "teachers");
+        }
+    }
+}
---
+++ codefirstproject/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace codefirstproject
+{
+    class Program
+    {
+       public class student
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public decimal GPA { get; set; }
+            public int age { get; set; }
+        }
+        public class teacher
+        {
+           public int id { get; set; }
+            public string name { get; set; }
+            public decimal Salary { get; set; }
+            public int age { get; set; }
+        }
+        public class SchoolContext : DbContext
+        {
+            public DbSet<student> students { get; set; }
+            public DbSet<teacher>teachers { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(@"server=DESKTOP-SFDKHS6\SQLEXPRESS;
+                                               database=SchoolTestdb;
+                                             Trusted_Connection=True;");
+            }
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+        }
+
+    }
+}
